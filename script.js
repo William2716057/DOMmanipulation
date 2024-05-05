d924af2ca41adaa0cceeabe156fe2462725e44fd
User <Willspri@gmail.com>
2024-05-06 11:29:08 +1200

    code clean

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('changeTextBtn').addEventListener('click', function() {
         var heading = document.querySelector('h1');
         heading.textContent = 'Well done!';
-//        heading.style.backgroundColor = "#FF6347"; // Change background color of h1
 	document.body.style.backgroundColor = "#FF6437";
     });
 });
