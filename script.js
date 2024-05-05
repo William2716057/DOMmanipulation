ad2da4b3f38ca8cc7cc67bfcc13190d3976bbb0b
User <Willspri@gmail.com>
2024-05-06 10:56:07 +1200

    updated style

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('changeTextBtn').addEventListener('click', function() {
         var heading = document.querySelector('h1');
-        heading.textContent = 'Hello, JavaScript!';
+        heading.textContent = 'Well done!';
     });
 });
+
